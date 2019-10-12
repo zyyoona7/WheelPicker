f71bbdfc789ca19601e124f94e95f702bb9174bb
zhangzhuoyue <zhangzhuoyue@ifaceparty.com>
2019-10-12 17:14:45 +0800

    增加 WheelYearView

---
+++ picker/src/main/java/com/zyyoona7/picker/ex/WheelYearView.kt
@@ -0,0 +1,15 @@
+package com.zyyoona7.picker.ex
+
+import android.content.Context
+import android.util.AttributeSet
+import com.zyyoona7.wheel.WheelViewKt
+
+class WheelYearView @JvmOverloads constructor(context: Context,
+                                attrs: AttributeSet? = null,
+                                defStyleAttr: Int = 0)
+    :WheelViewKt(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+}
